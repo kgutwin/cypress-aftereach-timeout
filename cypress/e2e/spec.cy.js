18f3f2ae818038eb57d71b300d48a75c44e85153
Karl Gutwin <karl@bioteam.net>
2024-09-06 09:37:43 -0400

    Add reproduction code

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,12 @@
 /// <reference types="cypress" />
-describe('page', () => {
-  it('works', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe("page", () => {
+  afterEach(() => {
+    cy.reproduce();
+  });
+
+  it("fails", () => {
+    // trigger a test failure
+    cy.contains("failure", { timeout: 500 }).should("exist");
+    // commenting out the above line causes the afterEach hook to succeed
+  });
+});
