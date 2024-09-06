b7b618d76ebd29eebfd335e3d55866543a64e616
Karl Gutwin <karl@bioteam.net>
2024-09-06 09:51:56 -0400

    simpler reproduction

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,7 +1,8 @@
 /// <reference types="cypress" />
 describe("page", () => {
   afterEach(() => {
-    cy.reproduce();
+    cy.visit("index.html");
+    cy.get('[data-cy="item"]', { timeout: 5000 }).should("exist");
   });
 
   it("fails", () => {
